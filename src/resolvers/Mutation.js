66b29b51def16255802e85d7db121eb259f1584b
Fábio Gonçalves <fabiobasquete4@hotmail.com>
2019-04-17 00:46:00 -0300

    Primeira versão

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,52 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET
+
+async function login(_, { email, password }, ctx, info) {
+
+  const user = await ctx.db.query.user({ where: { email } })
+  if (!user) {
+    throw new Error('Invalid credentials !')
+  }
+  console.log('User ', user)
+  const valid = await bcrypt.compare(password, user.password)
+  if (!valid) {
+    throw new Error('Invalid credentials !')
+  }
+
+  const token = jwt.sign({ userId: user.id },
+    JWT_SECRET,
+    { expiresIn: '2h' })
+
+  return {
+    token,
+    user
+  }
+
+}
+
+
+async function singup(_, args, ctx, info) {
+
+  const password = await bcrypt.hash(args.password, 10)
+  const user = await ctx.db.mutation.createUser({
+    data: {
+      ...args, password
+    }
+  })
+
+  const token = jwt.sign({ userId: user.id },
+    JWT_SECRET,
+    { expiresIn: '2h' })
+
+  return {
+    token,
+    user
+  }
+
+}
+
+module.exports = {
+  login,
+  singup
+}
---
+++ src/resolvers/Query.js
@@ -0,0 +1,10 @@
+const { getUserId } = require('./../utils')
+
+function user(parent, args, ctx, info) {
+  const userId = getUserId(ctx)
+  return ctx.db.query.user({ where: { id: userId } }, info)
+}
+
+module.exports = {
+  user
+}
